a293a8656b4cd97b5e795de5c3b4e0059f456034
kitchen-sys <kitchen@imsaindy.org>
2025-12-05 16:06:53 -0500

    Add cross-sectional factor and ML signal layer

--- finance_bot/__init__.py
+++ finance_bot/__init__.py
@@ -1,12 +1,15 @@
 """Finance bot package providing OT-driven modeling and live Discord scanning."""
 
 from .model import OTFinanceModel
+from .alpha import CrossSectionalSignalLayer, CrossSectionalMLForecaster
 from .scanner import YFinanceScanner
 from .discord_bot import create_bot
 from .config import BotConfig
 
 __all__ = [
     "OTFinanceModel",
+    "CrossSectionalSignalLayer",
+    "CrossSectionalMLForecaster",
     "YFinanceScanner",
     "create_bot",
     "BotConfig",
